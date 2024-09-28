8a8ba7a0f676f57d8fc3ea05f55d5c462056e3f4
Aline Soares <aliinanda@example.com>
2024-09-28 20:15:56 -0300

    atulaizaçao codigo

--- LeituradeDados.java
+++ LeituradeDados.java
@@ -1,28 +1,25 @@
 import java.util.Scanner;
 
 public class LeituradeDados {
+    
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Digite seu nome:");
-        String nome = scan.nextLine();  
-
-        System.out.println("Digite sua idade:");
-        int idade = scan.nextInt();  
-
-        System.out.println("Digite sua altura:");
-        float altura = scan.nextFloat();  
-
-        scan.nextLine();  
-
-        System.out.println("Você tem animal de estimação? (true/false)");
-        boolean temPet = scan.nextBoolean();  
-
-        System.out.println("\nSeu nome é: " + nome);
-        System.out.println("Sua idade é: " + idade);
-        System.out.println("Sua altura é: " + altura);
-        System.out.println("Tem pet: " + temPet);
-
-        scan.close();  
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Digite seu nome:");
+            String nome = scan.nextLine();
+            
+            System.out.println("Digite sua idade:");
+            int idade = scan.nextInt();
+            
+            System.out.println("Digite sua altura:");
+            float altura = scan.nextFloat();
+            
+            System.out.println("Voce tem animal de estimacao? (true/false)");
+            boolean temPet = scan.nextBoolean();
+            
+            System.out.println("\nSeu nome e: " + nome);
+            System.out.println("Sua idade e: " + idade);
+            System.out.println("Sua altura e: " + altura);
+            System.out.println("Tem pet: " + temPet);
+        }  
     }
 }
